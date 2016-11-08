45fbec7f360c445d6d6a7e9cb5d38e3723da3888
Dayanis Echeverria <dpecheverria48@misena.edu.co>
2016-11-08 09:19:10 -0500

    new file operadores aritmeticos

---
+++ operadores.rb
@@ -0,0 +1,26 @@
+class HelloWorld
+	def initialize()
+	end
+	def sal()
+		# operaciones 
+=begin
+suma = 2 + 3
+resta = 2 - 3
+mult = 2 * 3
+div = 2 / 3
+expo = 2**3
+puts suma
+puts resta
+puts mult
+puts div
+puts expo
+=end
+		variable = (14+2)/(2*2)
+		puts variable
+		#16
+
+	end
+end
+object = HelloWorld.new()
+object.sal
+gets()
